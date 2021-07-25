bbd411c4a9c232b506d8735cebb4288b12b14303
Thiti Dev <thiti.mwk.main@gmail.com>
2021-07-25 16:56:02 +0700

    implemented file reader on app entry [watching-initialization]

--- index.ts
+++ index.ts
@@ -1,5 +1,23 @@
 import './contribs/index';
 import { Logger } from './core/Logger';
 import { CORE_Samplers } from './provider';
+import fs from 'fs';
 
 Logger.debug(`Total of ${CORE_Samplers.total_exercise_loaded} exercises loaded from contributors`);
+
+fs.readFile('focus.txt', 'utf8', function (err, data) {
+    if (err) {
+        if (err.code === 'ENOENT') {
+            Logger.debug('No focus.txt found on this root directory, ignoring focusing process');
+            return;
+        } else {
+            throw err; // termimate application
+        }
+    }
+    startWatching(data);
+});
+
+function startWatching(key_name_pointer: string) {
+    Logger.debug('Watching -> ' + key_name_pointer);
+    CORE_Samplers.debug(key_name_pointer);
+}
